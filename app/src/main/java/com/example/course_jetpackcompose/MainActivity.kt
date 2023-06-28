26aa530eab6a020ef95fbac7906da63f343cff2a
Tristan DEVOILLE <tristan.devoille@diiage.org>
2023-06-28 15:42:37 +0200

    Fin de la 5ème vidéo

--- app/src/main/java/com/example/course_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/course_jetpackcompose/MainActivity.kt
@@ -32,65 +32,65 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val painter = painterResource(id = R.drawable.eminence_in_shadow)
-            val description = "The best"
-            val title = "The best of the best"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp))
-            ImageCard(
-                painter = painter,
-                contentDescription = description,
-                title = title)
-        }
-    }
-}
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_thin, FontWeight.Thin),
+            Font(R.font.lexend_light, FontWeight.Light),
+            Font(R.font.lexend_bold, FontWeight.Bold),
+            Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
+            Font(R.font.lexend_semibold, FontWeight.SemiBold),
+            Font(R.font.lexend_medium, FontWeight.Medium),
+            Font(R.font.lexend_extralight, FontWeight.ExtraLight)
+        )
 
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-){
-    Card (
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp)
-    ) {
-        Box(modifier = Modifier.height(200.dp)){
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
+        setContent {
             Box(modifier = Modifier
                 .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 300f
-                    )
-                )
+                .background(Color(0xFF101010)))
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Green,
+                            fontSize = 50.sp
+                        )
+                    ){
+                        append("J")
+                    }
+                    append("etpack ")
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Green,
+                            fontSize = 50.sp
+                        )
+                    ){
+                        append("C")
+                    }
+                    append("ompose")
+                },
+                color = Color.White,
+                fontSize = 30.sp,
+                fontFamily = fontFamily,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                textAlign = TextAlign.Center,
+                textDecoration = TextDecoration.Underline
             )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ){
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
         }
     }
 }
