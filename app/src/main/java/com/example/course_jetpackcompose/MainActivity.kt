9c46b49ca439fcc86a8274febcf2cc0b41b0e7f9
Tristan DEVOILLE <tristan.devoille@diiage.org>
2023-08-01 19:11:00 +0200

    Fin de la 16 ème vidéo avec un drop down fonctionnel

--- app/src/main/java/com/example/course_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/course_jetpackcompose/MainActivity.kt
@@ -44,10 +44,13 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -59,7 +62,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -67,7 +72,9 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.ContentScale
@@ -104,123 +111,88 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface (
-                color = Color(0xFF101010),
+            Surface(
+                color = Color(0xFF101010,),
                 modifier = Modifier.fillMaxSize()
             ) {
-              Box(
-                  contentAlignment = Alignment.Center
-              )  {
-                  Timer(totalTime = 100L * 1000L, handleColor = Color.Green, inactiveBarColor = Color.DarkGray, activeBarColor = Color(0xFF37B900), modifier = Modifier.size(200.dp))
-              }
+
+                DropDown(text = "Hello World", modifier = Modifier.padding(15.dp)
+                ) {
+                    Text(text = "This is now revealed!", modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .background(Color.Green))
+
+                }
             }
         }
     }
 }
 
 @Composable
-fun Timer(
-    totalTime: Long,
-    handleColor: Color,
-    inactiveBarColor: Color,
-    activeBarColor: Color,
+fun DropDown(
+    text: String,
     modifier: Modifier = Modifier,
-    initialValue: Float = 0f,
-    strokeWidth: Dp = 5.dp
+    initiallyOpened: Boolean = false,
+    content: @Composable () -> Unit
 ) {
-    var size by remember {
-        mutableStateOf(IntSize.Zero)
+    var isOpen by remember {
+        mutableStateOf(initiallyOpened)
     }
 
-    var value by remember {
-        mutableStateOf(initialValue)
-    }
-
-    var currentTime by remember {
-        mutableStateOf(totalTime)
-    }
-
-    var isTimerRunning by remember {
-        mutableStateOf(false)
-    }
-
-    LaunchedEffect(key1 = currentTime, key2 = isTimerRunning){
-        if(currentTime > 0 && isTimerRunning){
-            delay(100L)
-            currentTime -= 100L
-            value = currentTime / totalTime.toFloat()
-        }
-    }
-
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .onSizeChanged {
-                size = it
-            }
-    ){
-        Canvas(modifier = modifier){
-            drawArc(
-                color = inactiveBarColor,
-                startAngle = -215f,
-                sweepAngle = 250f,
-                useCenter = false,
-                size = Size(size.width.toFloat(),size.height.toFloat()),
-                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
-            )
-
-            drawArc(
-                color = activeBarColor,
-                startAngle = -215f,
-                sweepAngle = 250f * value,
-                useCenter = false,
-                size = Size(size.width.toFloat(),size.height.toFloat()),
-                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
-            )
-
-            val center = Offset(size.width / 2f, size.height / 2f)
-            val beta = (250f * value + 145f) * (PI / 180f).toFloat()
-            val r = size.width / 2f
-            val a = cos(beta) * r
-            val b = sin(beta) * r
-
-            drawPoints(
-                listOf(Offset(center.x+a,center.y+b)),
-                pointMode = PointMode.Points,
-                color = handleColor,
-                strokeWidth = (strokeWidth * 3f).toPx(),
-                cap = StrokeCap.Round
-            )
-        }
-        Text(
-            text = (currentTime / 1000L).toString(),
-            fontSize = 44.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Green
+    val alpha = animateFloatAsState(
+        targetValue = if(isOpen) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 300
         )
-        Button(
-            onClick = {
-                      if (currentTime <= 0L){
-                          currentTime = totalTime
-                          isTimerRunning = true
-                      } else {
-                          isTimerRunning = !isTimerRunning
-                      }
-            },
-            modifier = Modifier.align(Alignment.BottomCenter),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = if(!isTimerRunning || currentTime <= 0L){
-                    Color.Green
-                } else {
-                    Color.Red
-                }
-            )
+    )
+
+    val rotateX = animateFloatAsState(
+        targetValue = if(isOpen) 0f else -90f,
+        animationSpec = tween(
+            durationMillis = 300
+        )
+    )
+
+    Column (
+        modifier = modifier
+            .fillMaxWidth()
             ) {
-                Text(
-                    text = if (isTimerRunning && currentTime >= 0L) "Stop"
-                    else if (!isTimerRunning && currentTime >= 0L) "Start"
-                    else "Restart"
-                )
+        Row (
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                ) {
+            Text(
+                text = text,
+                color = Color.White,
+                fontSize = 16.sp
+            )
+
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Open or close the drop down",
+                tint = Color.White,
+                modifier = Modifier
+                    .clickable {
+                        isOpen = !isOpen
+                    }
+                    .scale(1f, if (isOpen) -1f else 1f)
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .graphicsLayer {
+                    transformOrigin = TransformOrigin(0.5f, 0f)
+                    rotationX = rotateX.value
+                }
+                .alpha(alpha.value)
+        ){
+            content()
         }
     }
 }
